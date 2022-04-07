e8f552d4a451d12d84a4526bcfbd2f1a1d7b72b0
dennistarsang <sangdennis9@gmail.com>
2022-04-07 12:32:03 +0300

    FIXED: Added a resolve function to the query macro.

--- lib/community_web/schema.ex
+++ lib/community_web/schema.ex
@@ -11,6 +11,8 @@ defmodule CommunityWeb.Schema do
 
   query do
     @desc "Get all links"
-    field :all_links, non_null(list_of(non_null(:link)))
+    field :all_links, non_null(list_of(non_null(:link))) do
+      resolve(&NewsResolver.all_links/3)
+    end
   end
 end
